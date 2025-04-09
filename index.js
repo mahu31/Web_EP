b0104bb7e2aaf7d1ebfaaa95f700d76ad4174705
Valentin Mahu <mahu31valentin@gmail.com>
2025-04-09 13:34:09 +0300

    second task added

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+// Task 1
 const celsiusToFahrenheit = (celsius) =>  (celsius * 9/5) + 32
-
 console.log(celsiusToFahrenheit(0))
+
+// Task 2
+const isPrime = (n) => {
+    if (n <= 1) return false;
+    for (let i = 2; i < n; i++) {
+        if (n % i === 0) {
+            return false;
+        }
+    }
+    return true;
+};
+
+console.log(isPrime(2));
+
+
